e53bf1bb63be0ca70b81c4df9e7df06b127fe518
Ron Gould <rgould3@kent.edu>
2022-06-05 17:20:45 -0400

    did some testing

--- GenerateMacAddresses.sh
+++ GenerateMacAddresses.sh
@@ -14,7 +14,7 @@ RANDMACS=$( openssl rand -hex $(( NumOfMacsToGenerate * 3 )) )
 
 while [ $iter -lt $NumOfMacsToGenerate ]
 do
-	offst=$(( iter * 3 ))
+	offst=$(( iter * 6 ))
 	MACADDRESS="${OUI}${RANDMACS:${offst}:6}"
 	printf "${MACADDRESS:0:2}:${MACADDRESS:2:2}:${MACADDRESS:4:2}:${MACADDRESS:6:2}:${MACADDRESS:8:2}:${MACADDRESS:10:2}\n"
 	iter=$((iter + 1))
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is to generate MAC addresses for xcp-ng VMs.
 
 The OUI prefix is Ascii "Xen", "58:65:6e". This is set with a variable in the script. The sole optional argument is the number of MAC Addresses to create. `openssl` is required.
 
+## Note that this doesn't check for duplicate MACs.
+
+
